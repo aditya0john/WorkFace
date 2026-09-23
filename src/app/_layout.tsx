e54191226f730e504de5d8f61830c809f9e6f4b3
Aditya <johnaditya46@gmail.com>
2026-09-23 13:10:30 +0530

    first commit

---
+++ src/app/_layout.tsx
@@ -0,0 +1,24 @@
+import { Stack } from "expo-router";
+
+export default function RootLayout() {
+  return (
+    <Stack
+      screenOptions={{
+        headerStyle: { backgroundColor: "transparent" },
+        headerTitleAlign: "center",
+      }}
+    >
+      <Stack.Screen name="index" options={{ title: "index", headerShown: true }} />
+      <Stack.Screen name="Modal/CameraCaptureModal"
+        options={{
+          title: "Camera Capture",
+          headerShown: false,
+          gestureDirection: "vertical",
+          presentation: "formSheet",
+          animation: "slide_from_bottom",
+          sheetAllowedDetents: [0.8],
+          sheetCornerRadius: 42,
+        }} />
+    </Stack>
+  )
+}
